6f36b8a950c035280f187aff17418f99ee0ae802
Daniel Salamon <salidani@gmail.com>
2020-11-16 09:14:41 +0100

    Update aes-256-cbc.js

--- aes-256-cbc.js
+++ aes-256-cbc.js
@@ -1,51 +1,58 @@
 'use strict';
 
 const log = (name, value) =>
-  console.log(name.padStart(20, ' ') + ': ' + (value || 'falsy').toString());
+  console.log(name.padStart(20, ' ') + ': ', (value || 'falsy'));
 
 const crypto = require('crypto');
 const ALGORITHM = 'aes-256-cbc';
 const KEY = crypto.randomBytes(16).toString('hex'); // set random encryption key
-const IV = crypto.randomBytes(8).toString('hex'); // set random initialisation vector
 
 const data = {
   session: crypto.randomBytes(2).toString('hex'),
   user: crypto.randomBytes(2).toString('hex'),
 };
-const phrase = JSON.stringify(data);
 log('ALGORITHM', ALGORITHM);
 log('KEY', KEY);
-log('IV', IV);
-log('phrase', phrase);
+console.log('–'.repeat(60));
+
+log('original data', data);
+
+const phrase = JSON.stringify(data);
+log('original JSON', phrase);
 
 /**
- * Encrypts an input string using with the given algorithm, secret key and initialisation vector
+ * Encrypts an input string using with the given algorithm, secret key
  * @param input plain text
  * @returns {string} encrypted string
  */
 const encrypt = (input) => {
-  let cipher = crypto.createCipheriv(ALGORITHM, KEY, IV);
-  return cipher.update(input, 'utf8', 'base64') + cipher.final('base64');
+  const initialisationVector = crypto.randomBytes(8).toString('hex');
+  let cipher = crypto.createCipheriv(ALGORITHM, KEY, initialisationVector);
+  return `${cipher.update(input, 'utf8', 'base64') + cipher.final('base64')}:${initialisationVector}`;
 };
 
 /**
- * Decrypts an encrypted string with the given secret key and initialisation vector
+ * Decrypts an encrypted string with the given secret key
  * @param encrypted
- * @returns {string}
+ * @returns {string} decrypted plain text
  */
 const decrypt = (encrypted) => {
-  let decipher = crypto.createDecipheriv(ALGORITHM, KEY, IV);
-  return decipher.update(encrypted, 'base64', 'utf8') + decipher.final('utf8');
+  const [encoded, initialisationVector] = encrypted.split(':');
+  let decipher = crypto.createDecipheriv(ALGORITHM, KEY, initialisationVector);
+  return decipher.update(encoded, 'base64', 'utf8') + decipher.final('utf8');
 };
 
-let encrypted_key = encrypt(phrase);
-log('encrypted_key', encrypted_key);
+let encrypted = encrypt(phrase);
+log('encrypted', encrypted);
 
-let url_encoded = encodeURIComponent(encrypted_key);
-log('url_encoded', url_encoded);
+let urlEncoded = encodeURIComponent(encrypted);
+log('url encoded', urlEncoded);
 
-let url_decoded = decodeURIComponent(url_encoded);
-log('url_decoded', url_decoded);
+let urlDecoded = decodeURIComponent(urlEncoded);
+log('url decoded', urlDecoded);
 
-let decoded_phrase = decrypt(url_decoded);
-log('decoded_phrase', decoded_phrase);
+let decodedPhrase = decrypt(urlDecoded);
+log('decoded JSON', decodedPhrase);
+
+let decoded = JSON.parse(decodedPhrase);
+log('decoded data', decoded);
