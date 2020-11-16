b57fbd98b9024ce4622255bf1f0453d11378754d
Daniel Salamon <salidani@gmail.com>
2020-11-16 10:53:04 +0100

    Moved algorithm and key to encrypt/decrypt function parameters

--- aes-256-cbc.js
+++ aes-256-cbc.js
@@ -23,26 +23,30 @@ log('original JSON', phrase);
 /**
  * Encrypts an input string using with the given algorithm, secret key
  * @param input plain text
+ * @param algorithm to be used for encryption
+ * @param key secret key for encryption
  * @returns {string} encrypted string
  */
-const encrypt = (input) => {
+const encrypt = (input, algorithm, key) => {
   const initialisationVector = crypto.randomBytes(8).toString('hex');
-  let cipher = crypto.createCipheriv(ALGORITHM, KEY, initialisationVector);
+  let cipher = crypto.createCipheriv(algorithm, key, initialisationVector);
   return `${cipher.update(input, 'utf8', 'base64') + cipher.final('base64')}:${initialisationVector}`;
 };
 
 /**
  * Decrypts an encrypted string with the given secret key
  * @param encrypted
+ * @param algorithm to be used for encryption
+ * @param key secret key for encryption
  * @returns {string} decrypted plain text
  */
-const decrypt = (encrypted) => {
+const decrypt = (encrypted, algorithm, key) => {
   const [encoded, initialisationVector] = encrypted.split(':');
-  let decipher = crypto.createDecipheriv(ALGORITHM, KEY, initialisationVector);
+  let decipher = crypto.createDecipheriv(algorithm, key, initialisationVector);
   return decipher.update(encoded, 'base64', 'utf8') + decipher.final('utf8');
 };
 
-let encrypted = encrypt(phrase);
+let encrypted = encrypt(phrase, ALGORITHM, KEY);
 log('encrypted', encrypted);
 
 let urlEncoded = encodeURIComponent(encrypted);
@@ -51,24 +55,8 @@ log('url encoded', urlEncoded);
 let urlDecoded = decodeURIComponent(urlEncoded);
 log('url decoded', urlDecoded);
 
-let decodedPhrase = decrypt(urlDecoded);
+let decodedPhrase = decrypt(urlDecoded, ALGORITHM, KEY);
 log('decoded JSON', decodedPhrase);
 
 let decoded = JSON.parse(decodedPhrase);
 log('decoded data', decoded);
-
-/*  
-
-Example output:
-
-           ALGORITHM:  aes-256-cbc
-                 KEY:  ff822270efbbb07773c48ea81c90120f
-––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-       original data:  { session: '1ec0', user: 'eadc' }
-       original JSON:  {"session":"1ec0","user":"eadc"}
-           encrypted:  tPgImxgyvYm24bXOwKPDyz/U3RLUsBC7tMEubztjklAMmzT1Bb+Gzo+kmKQkf/G4:6b151156943e0dea
-         url encoded:  tPgImxgyvYm24bXOwKPDyz%2FU3RLUsBC7tMEubztjklAMmzT1Bb%2BGzo%2BkmKQkf%2FG4%3A6b151156943e0dea
-         url decoded:  tPgImxgyvYm24bXOwKPDyz/U3RLUsBC7tMEubztjklAMmzT1Bb+Gzo+kmKQkf/G4:6b151156943e0dea
-        decoded JSON:  {"session":"1ec0","user":"eadc"}
-        decoded data:  { session: '1ec0', user: 'eadc' }
-*/
